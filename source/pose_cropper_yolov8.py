a1d2ab49719f3ddaeda8e5d159aac71ed6f6cc95
Baturalp Kabadayi <baturalparslann@hotmail.com>
2023-08-06 18:32:44 +0300

    Implement person cropper for data labeling

--- source/pose_cropper_yolov8.py
+++ source/pose_cropper_yolov8.py
@@ -4,10 +4,15 @@ from ultralytics import YOLO
 import time
 import copy
 
-VIDEO_PATH = "../data/akcansa_share/video0.mp4"
+VIDEO_NAME = "betonsa_2"
+VIDEO_PATH = f"../data/akcansa_share/{VIDEO_NAME}.mp4"
 # VIDEO_PATH = "../data/train6_nohelmet.jpg"
+
+CROP_AND_SAVE = True  # True if you want to crop and save body parts
+SAVE_PATH = f"../data/cropped_parts/{VIDEO_NAME}"
+
 FRAME_COUNT = 1000
-FRAME_RATE = 10
+FRAME_RATE = 20
 
 if __name__ == "__main__":
     model = YOLO("yolov8n-pose.pt")
@@ -15,7 +20,9 @@ if __name__ == "__main__":
 
     for i in range(FRAME_COUNT):
         success, frame = video.read()
-
+        # Continue until desired frame rate.
+        if CROP_AND_SAVE and (i % FRAME_RATE != 0):
+            continue
         if success:
             results = model(frame)
 
@@ -26,9 +33,9 @@ if __name__ == "__main__":
 
             # annotated_frame = results[0].plot()
             annotated_frame = copy.deepcopy(frame)
-            for i, box in enumerate(boxes):
+            for j, box in enumerate(boxes):
                 x1, y1, x2, y2 = int(box[0]), int(box[1]), int(box[2]), int(box[3])
-                current_keypoints = keypoints[i]
+                current_keypoints = keypoints[j]
 
                 # Crop head part
                 top_y = int(y1)
@@ -73,8 +80,23 @@ if __name__ == "__main__":
                     left_wrist[1] + hand_radius
                 )
 
-                # Draw rectangles
+                # Crop and save persons from images
+                # Expand the person according to expand constant.
+                scale_expand = 0.1
+                len_expand_y = (y2 - y1) * scale_expand
+                len_expand_x = (x2 - x1) * scale_expand
+                y1_expanded = int(y1 - len_expand_y)
+                y2_expanded = int(y2 + len_expand_y)
+                x1_expanded = int(x1 - len_expand_x)
+                x2_expanded = int(x2 + len_expand_x)
 
+                # Crop and save the image.
+                person_cropped = annotated_frame[
+                    y1_expanded:y2_expanded, x1_expanded:x2_expanded
+                ]
+                cv2.imwrite(f"{SAVE_PATH}/person_{i}_{j}.jpg", person_cropped)
+
+                # Draw rectangles
                 # head
                 cv2.rectangle(
                     annotated_frame,
