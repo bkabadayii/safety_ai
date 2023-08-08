3844a18c24873766bce0bc230e6ce0eeab962657
Dogukan Atakur <datakur1@binghamton.edu>
2023-08-08 14:58:19 +0300

    now, predict function also returns probabilities, and it uses 2 different models for detection based on the target class.

--- source/object_detector.py
+++ source/object_detector.py
@@ -1,14 +1,62 @@
 from ultralytics import YOLO
 
-safety_model = YOLO("../object_detector_model_training/models/C/weights/C.pt")
+modelC = YOLO("../model_training/models/C/weights/C.pt")
+modelV = YOLO("../model_training/models/V/weights/V.pt")
+
 threshold = 0.40
+
+# Both of the 'run' functoins may also return the whole list of probabilities, and the class ids for images that contain multiple person
+# currently, it assumes that img contains maximum 1 person in the frame. (our current solution)
+# You can also use predictLegacy() for the images that contain multiple person for now.
 
 def predict(img, targetClassName, threshold=threshold):
     
-    results = safety_model(img)
+    if(targetClassName=="vest"):
+        results = runVestModel(img)
+    else:
+        results = runHelmetModel(img)
+    
+    return results # (Boolean, Float)
+
+def runVestModel(img):
+    
+    results = modelV(img)
+    
+    for res in results:
+        if(len(res.boxes.cls)>0):
+            vest_status = res.boxes.cls[0].item() == 1
+            status_prob = res.boxes.conf[0]
+        else:
+            vest_status = False
+            status_prob = 0.0
+    
+        return (vest_status, status_prob)
+
+    return (False, 0.0)
+
+def runHelmetModel(img):
+    
+    
+    results = modelC(img)
+    
+    for res in results:
+        if(len(res.boxes.cls)>0):
+            helmet_status = res.boxes.cls[0].item() == 0 # 0 for 'helmet' and 1 for 'vest' in modelC.
+            status_prob = res.boxes.conf[0]
+        else:
+            helmet_status = False
+            status_prob = 0.0
+        
+        return (helmet_status, status_prob)
+    
+    return (False, 0.0)
+
+# UNUSED
+def predictLegacy(img, targetClassName, threshold=threshold):
+    
+    results = modelC(img)
     
     classNames = ["helmet", "vest"]
-
     for res in results:
         
         classIDs = res.boxes.cls
@@ -18,9 +66,13 @@ def predict(img, targetClassName, threshold=threshold):
             
             classID = int(classID.item())
             
-            if(classNames[classID] == targetClassName):      
-                prob = prob.item()
-                #print(f"DETECTED --- Class: {classNames[int(classID)]}, Confidence: {prob}")
-                if(prob>threshold): return True
+            if(classNames[classID] == targetClassName):                      
+                #Debug #print(f"DETECTED --- Class: {classNames[int(classID)]}, Confidence: {prob}")
+                
+                prob = prob.item()  
+                if(prob > threshold):
+                    return (True, prob)
+                else:
+                    return (False, prob)
     
-    return False
+    return (False, 0)
